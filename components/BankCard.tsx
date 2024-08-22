2a9776a8948d092d1fe03a0c00b61c27ae83e116
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2024-08-22 14:14:40 -0400

    Implemnt sign in page UI

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -8,7 +8,7 @@ const BankCard = ({account,userName,showBalance=true}:CreditCardProps) => {
     <div className="flex flex-col">
       <Link href="/" className='bank-card'>
         <div className="bank-card_content">
-          <div className="">
+          <div>
             <h1 className="text-16 font-semibold text-white">
               {account.name || userName}
             </h1>
