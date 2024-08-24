b491db42c2b5337ef85264ca3e6ecb19aecce231
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2024-08-24 14:23:52 -0400

    Implement plaid for transaction

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -14,6 +14,7 @@ import { Loader2 } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import { signUp } from '@/lib/actions/user.actions'
 import SignIn from '@/app/(auth)/sign-in/page'
+import PlaidLink from './PlaidLink'
 
 const AuthForm = ({type}:{type:string}) => {
   const router = useRouter();
@@ -78,11 +79,11 @@ const AuthForm = ({type}:{type:string}) => {
         </div>
         </header>
 
-        {user ? (
+        {/* {user ? ( */}
           <div className="flex flex-col gap-4">
-            {/* Plaid link */}
+            <PlaidLink user={user} variant="primary"/>
           </div>
-        ):(
+        {/* ):( */}
           <>
             <Form {...form}>
               <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -129,7 +130,7 @@ const AuthForm = ({type}:{type:string}) => {
               </Link>
             </footer>
           </>
-        )}
+        {/* )} */}
       </section>
   )
 }
